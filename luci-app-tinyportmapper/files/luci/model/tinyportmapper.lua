91a58e89b8bbae0cccedb90a0b0c3d0a742d2d45
nlroel(WSL) <lroel@buaa.edu.cn>
2022-01-04 15:56:37 +0800

    update

--- luci-app-tinyportmapper/files/luci/model/tinyportmapper.lua
+++ luci-app-tinyportmapper/files/luci/model/tinyportmapper.lua
@@ -22,9 +22,9 @@ end
 function get_version(name)
    local info = luci.util.split(luci.sys.exec("%s -h 2>/dev/null" %{name}), "\n")
    if table.getn(info) > 3 then
-      local version = string.match(info[2], "git version:%s*(%w+)")
-      local build = string.match(info[2], "build date:%s(.+)")
-      return info[1] == "tinyPortMapper" and version or "", info[1] == "tinyPortMapper" and build or ""
+      local version = string.match(info[3], "git version:%s*(%w+)")
+      local build = string.match(info[3], "build date:%s(.+)")
+      return info[2] == "tinyPortMapper" and version or "", info[2] == "tinyPortMapper" and build or ""
    else
       return "",""
    end
